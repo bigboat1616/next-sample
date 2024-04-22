8170560ac744ee99c1480d3e621b8765a9e3ba70
bigboat1616 <taishu.arashima.anzu@au.com>
2024-04-22 20:06:00 +0900

    入力テキスト変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+const Card = () => {
+  return <div>aaaaa</div>
+}
+
 export default function Home() {
   return (
     <div>
-      <p>Next sample</p>
+      <p>いい感じ！！！</p>
+      <Card />
     </div>
   )
 }
