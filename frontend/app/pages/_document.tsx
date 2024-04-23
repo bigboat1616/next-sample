1211dd0d93212aef1ceb73eeaddc951dc6d733e7
bigboat1616 <taishu.arashima.anzu@au.com>
2024-04-23 11:19:02 +0900

    追加部分のコメントアウト

--- frontend/app/pages/_document.tsx
+++ frontend/app/pages/_document.tsx
@@ -1,27 +1,27 @@
-// src/pages/_document.tsx
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+// // src/pages/_document.tsx
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-    render() {
-        const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
+// class MyDocument extends Document {
+//     render() {
+//         const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY;
         
-        return (
-        <Html>
-            <Head>
-              {googleMapsApiKey && (
-                  <script
-                      async
-                      src={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places`}
-                  />
-              )}
-            </Head>
-            <body>
-            <Main />
-            <NextScript />
-            </body>
-        </Html>
-        );
-    }
-}
+//         return (
+//         <Html>
+//             <Head>
+//               {googleMapsApiKey && (
+//                   <script
+//                       async
+//                       src={`https://maps.googleapis.com/maps/api/js?key=${googleMapsApiKey}&libraries=places`}
+//                   />
+//               )}
+//             </Head>
+//             <body>
+//             <Main />
+//             <NextScript />
+//             </body>
+//         </Html>
+//         );
+//     }
+// }
   
-export default MyDocument;
+// export default MyDocument;
