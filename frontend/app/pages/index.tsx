455b29147f335fa1a1ac6c2f0816b0e30d73321e
bigboat1616 <taishu.arashima.anzu@au.com>
2024-04-23 16:11:36 +0900

    root環境変更

--- frontend/app/pages/index.tsx
+++ frontend/app/pages/index.tsx
@@ -1,14 +1,14 @@
-// // src/pages/index.tsx
-// import React from 'react';
-// import GoogleMap from '../components/GoogleMap';
+// src/pages/index.tsx
+import React from 'react';
+import GoogleMap from '../components/Googlemap';
 
-// const Home: React.FC = () => {
-//   return (
-//     <section>
-//       <h1 className="mb-5">Google Maps with Next.js and TypeScript</h1>
-//       <GoogleMap />
-//     </section>
-//   );
-// };
+const Home: React.FC = () => {
+  return (
+    <section>
+      <h1 className="mb-5">Google Maps with Next.js and TypeScript</h1>
+      <GoogleMap />
+    </section>
+  );
+};
 
-// export default Home;
+export default Home;
