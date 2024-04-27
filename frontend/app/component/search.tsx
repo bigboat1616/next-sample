eefeb402e660d2b128e469ef1059c788285975f9
Circ-Leaf <miraclesp1@gmail.com>
2024-04-28 05:28:29 +0900

    混雑度カラーピン出力可能

--- frontend/app/component/search.tsx
+++ frontend/app/component/search.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { GoogleMap, Marker, useJsApiLoader} from '@react-google-maps/api';
 // import getNearFood from './getNearFood';
 let Map: google.maps.Map;
+const googleMapsLibraries : Array<"places"> = ['places'];
 let place_ids: string[] = [];
 const containerStyle = {
   width: '1200px',
@@ -19,7 +20,7 @@ function GeocodeComponent() {
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY!,
-        libraries : ['places'],
+        libraries : googleMapsLibraries,
     });
     const [address, setAddress] = useState('');
     const [location, setLocation] = useState<MarkerPoint | null>(null);
@@ -83,9 +84,26 @@ function GeocodeComponent() {
        *
        */
       function callback(result: any, status: any) {
-        var service = new google.maps.places.PlacesService(Map);
+        // var service = new google.maps.places.PlacesService(Map);
         let pointLists = [];
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < result.length; i++) {
+            // createMarker(result[i], 0, 255, 0);
+            pointLists.push({lat: result[i].geometry.location.lat(), lng: result[i].geometry.location.lng()});
+            // var request2 = {
+            //           placeId: result[i].place_id,
+            //           fields: ['name','reviews']
+            //         };
+            // service.getDetails(request2, callback2);
+          }
+        }
+        console.log('pointLists:', pointLists);
+        sendBackend(pointLists);
+        return;
+      }
+      function sendBackend(pointLists: MarkerPoint[]) {
         // バックエンドに座標を送信
+        type RGB = {r: number, g: number, b: number};
         const sendCoordinatesToBackend = async () => {
           try {
             const response = await fetch('http://localhost:5000/save_coordinates', {
@@ -98,41 +116,39 @@ function GeocodeComponent() {
             if (!response.ok) {
               throw new Error(`HTTP error! status: ${response.status}`);
             }
+            const data = await response.json(); // JSONレスポンスをパース
+            console.log('data######:', data);
+            console.log('data:', data[0].location);
+            console.log('data:', data[0].location.lat);
+            console.log('data:', data[0].rgb);
             console.log('Coordinates sent successfully');
+            // バックエンドから受け取ったRGB値でマーカーを更新
+              for (let i = 0; i < data.length; i++) {
+                createMarker(data[i].location, 0, data[i].rgb , 0);
+              }
           } catch (error) {
             console.error('Failed to send coordinates to backend', error);
           }
         };
-       // バックエンドに座標を送信↑
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < result.length; i++) {
-            createMarker(result[i]);
-            pointLists.push({lat: result[i].geometry.location.lat(), lng: result[i].geometry.location.lng()});
-            var request2 = {
-                      placeId: result[i].place_id,
-                      fields: ['name','reviews']
-                    };
-            service.getDetails(request2, callback2);
-          }
-        }
-        console.log('place_id:', pointLists);
+        console.log('pointLists:!!!', pointLists);
         setCoordinatesList(pointLists);
         sendCoordinatesToBackend();
+       // バックエンドに座標を送信↑
         return;
       }
-      function callback2(result: any, status: any) {
-        let average = 0;
-        console.log('details:', result);
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < result.reviews.length; i++) {
-                average += result.reviews[i].rating;
-                // console.log('details:', result.reviews[i].rating, result.reviews[i].text);
-            }
-            average = average / result.reviews.length;
-            // console.log('average:', average);
-        }
-        return;
-        }
+      // function callback2(result: any, status: any) {
+      //   let average = 0;
+      //   console.log('details:', result);
+      //   if (status == google.maps.places.PlacesServiceStatus.OK) {
+      //       for (var i = 0; i < result.reviews.length; i++) {
+      //           average += result.reviews[i].rating;
+      //           // console.log('details:', result.reviews[i].rating, result.reviews[i].text);
+      //       }
+      //       average = average / result.reviews.length;
+      //       // console.log('average:', average);
+      //   }
+      //   return;
+      //   }
         // /**place_idを用いて詳細情報を取得 */
         // function getDetails(place_ids: string[]) {
         //     var service = new google.maps.places.PlacesService(Map);
@@ -145,17 +161,39 @@ function GeocodeComponent() {
        * 検索結果の箇所にマーカーを設定する
        *
        */
-      function createMarker(place: google.maps.places.PlaceResult) {
-        if (!place.geometry || !place.geometry.location) return;
+      function createMarker(place :MarkerPoint, r : number, g : number, b : number) {
+        // RGB値で色を指定するSVGアイコン
+        const svgMarker = {
+            path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
+            fillColor: `rgb(${r},${g},${b})`, // RGBで色指定
+            fillOpacity: 1,
+            strokeWeight: 0,
+            rotation: 0,
+            scale: 2,
+            anchor: new google.maps.Point(15, 30),
+        };
         // お店情報マーカー
-        const marker = new google.maps.Marker({
-          map: Map,
-          position: place.geometry.location,
-          title: place.name,
-          label: place.name?.substr(0, 1),
-          optimized: false,
+        const markers = new google.maps.Marker({
+            map: Map,
+            position: place,
+            // position: place.geometry.location,
+            // title: place.name,
+            // label: place.name?.substr(0, 1),
+            optimized: false,
+            icon: svgMarker,  // カスタムアイコンを使用
         });
     }
+    //   function createMarker(place: google.maps.places.PlaceResult) {
+    //     if (!place.geometry || !place.geometry.location) return;
+    //     // お店情報マーカー
+    //     const markers = new google.maps.Marker({
+    //       map: Map,
+    //       position: place.geometry.location,
+    //       title: place.name,
+    //       label: place.name?.substr(0, 1),
+    //       optimized: false,
+    //     });
+    // }
     return (
         <div>
             {isLoaded ? (
@@ -183,7 +221,7 @@ function GeocodeComponent() {
                   onLoad={onLoad}
                   options={options}
                 >
-                <Marker position = {markers}/>
+                {/* <Marker position = {markers}/> */}
                   {/* {location && <Marker position={location} />} */}
                   {/* 上は検索した一点のみ表示，下は検索した周辺の飲食店を一括表示 */}
                   {/* {markers.map((marker, index) => (
